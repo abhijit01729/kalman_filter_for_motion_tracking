494394c750d8a7150df76a45b98a8c6a863fe26d
Abhijit <abhijitbanerjee2297@gmail.com>
2024-09-14 11:43:41 +0530

    C++ skeleton class creation for Kalman filter and integrate with tests

--- python/test_kf.py
+++ python/test_kf.py
@@ -1,13 +1,30 @@
-from kf import KF
+import kf as python_kf
+
+# == Import CPP wrapper .so file == #
+import os
+cur_file_path = os.path.dirname(os.path.abspath(__file__))
+
+import sys
+sys.path.append(cur_file_path + '/../cpp/build')
+
+import kf_cpp as cpp_kf
+# == End import CPP wraper .so file == #
+
 import numpy as np
 import unittest
+from parameterized import parameterized_class
+
+@parameterized_class([
+   { "KF": python_kf.KF },
+   { "KF": cpp_kf.KF}
+])
 
 class TestKF(unittest.TestCase):
     def test_can_construct_with_x_and_v(self):
         x = 0.2
         v = 2.3
 
-        kf = KF(x, v, 1.2)
+        kf = self.KF(x, v, 1.2)
         self.assertAlmostEqual(kf.pos, x)
         self.assertAlmostEqual(kf.vel, v)
 
@@ -15,7 +32,7 @@ class TestKF(unittest.TestCase):
         x = 0.2
         v = 2.3
 
-        kf = KF(x, v, 1.2)
+        kf = self.KF(x, v, 1.2)
         kf.predict(0.1)
 
         self.assertEqual(kf.cov.shape, (2, 2))
@@ -25,7 +42,7 @@ class TestKF(unittest.TestCase):
         x = 0.2
         v = 2.3
 
-        kf = KF(x, v, 1.2)
+        kf = self.KF(x, v, 1.2)
 
         for i in range(10):
             det_before = np.linalg.det(kf.cov)
@@ -39,7 +56,7 @@ class TestKF(unittest.TestCase):
         x = 0.2
         v = 2.3
 
-        kf = KF(x, v, 1.2)
+        kf = self.KF(x, v, 1.2)
 
         det_before = np.linalg.det(kf.cov)
         kf.update(0.1, 0.01)
